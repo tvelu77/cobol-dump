b1952d2816b2377e699e654234773902a50ab6be
Thomas VELU <106349820+tvelu77@users.noreply.github.com>
2024-11-12 15:58:51 +0100

    add : hello world program

---
+++ FromZeroToCobol/hello-world.cbl
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWORLD.
+       AUTHOR. Thomas VELU.
+
+       PROCEDURE DIVISION.
+           
+           DISPLAY "Hello world !".
+           
+           STOP RUN.
+      
